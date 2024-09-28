ecace02731b6d7627161e0b4de4bc663f3c37123
Harsh Sharma <harshsharma0009@gmail.com>
2024-09-27 22:47:35 -0400

    NavBar completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'resume', component: ResumeComponent},
+    {path: 'portfolio', component: PortfolioComponent},
+    {path: '**', redirectTo: '', component: HomeComponent, pathMatch: 'full'}
+];
