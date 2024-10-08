d8c9af187b343f4eef5c92c85f55e1f30939e1ed
Harsh Sharma <harshsharma0009@gmail.com>
2024-10-08 02:06:35 -0400

    images upadted

--- src/app/_models/TagModel.ts
+++ src/app/_models/TagModel.ts
@@ -1,19 +1,21 @@
 export class Tag {
-    static readonly ANGULAR = new Tag('Angular', 'red');
-    static readonly TYPESCRIPT = new Tag('TypeScript', 'darkred');
-    static readonly PYTHON = new Tag('Python', 'pink');
-    static readonly CSHARP = new Tag('C#', 'green');
-    static readonly JAVA = new Tag('Java', 'orange');
-    static readonly NODEJS = new Tag('Node.JS', 'brown');
-    static readonly ASPNET = new Tag('ASP.NET', 'purple');
-    static readonly JAVASCRIPT = new Tag('JavaScript', 'Orange');
-    static readonly REACT = new Tag('React', 'blue');
-    
-    private constructor(private readonly key: string, public readonly color: string){
+  static readonly ANGULAR = new Tag('Angular', '#BDAA00');
+  static readonly TYPESCRIPT = new Tag('TypeScript', 'darkred');
+  static readonly JAVASCRIPT = new Tag('JavaScript', 'pink');
+  static readonly AEM = new Tag('Adobe Experience Manager', 'red');
+  static readonly CSS = new Tag('CSS', 'green');
+  static readonly JAVA = new Tag('Java', 'orange');
+  static readonly NODEJS = new Tag('Node.JS', 'brown');
+  static readonly HTML = new Tag('HTML', 'purple');
+  static readonly REACT = new Tag('React', 'blue');
+  static readonly HTL = new Tag('Sightly', 'violet');
 
-    }
+  private constructor(
+    private readonly key: string,
+    public readonly color: string
+  ) {}
 
-    toString(){
-        return this.key;
-    }
+  toString() {
+    return this.key;
+  }
 }
--- src/app/_services/projects-service.ts
+++ src/app/_services/projects-service.ts
@@ -3,29 +3,43 @@ import { Project } from '../_models/ProjectCardModel';
 import { Tag } from '../_models/TagModel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectsService {
-
   projects: Project[] = [
-    {id: 0, name: "Sample Python Project", pictures: ['image1.jpg', 'image2.jpg', 'image3.jpg'], projectLink: "//www.github.com", summary: "Python project that analyzes stock market data.", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.PYTHON]},
-    {id: 1, name: "Sample Angular App", pictures: ['image1.jpg', 'image2.jpg', 'image3.jpg'], projectLink: "//www.github.com", summary: "Fullstack web app developed using Angular and Node.JS", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.ANGULAR, Tag.TYPESCRIPT, Tag.NODEJS]},
-    {id: 2, name: "Sample .Net App", pictures: ['image1.jpg', 'image2.jpg', 'image3.jpg'], projectLink: "//www.github.com", summary: "Fullstack web app developed using React and ASP.NET", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.REACT ,Tag.CSHARP, Tag.ASPNET]},
-    {id: 3, name: "Web API Project", pictures: ['image1.jpg', 'image2.jpg', 'image3.jpg'], projectLink: "//www.github.com", summary: "Web API Project that was developed for a class project.", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.CSHARP, Tag.ASPNET]},
-    {id: 4, name: "Chrome Extension", pictures: ['image1.jpg', 'image2.jpg', 'image3.jpg'], projectLink: "//www.github.com", summary: "Developed a chrome extension that tracks the prices of furniture.", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.JAVASCRIPT]},
-    {id: 5, name: "Mobile App", pictures: ['image1.jpg', 'image2.jpg', 'image3.jpg'], projectLink: "//www.github.com", summary: "Mobile app developed in java that tracks the departure and arrival of trains.", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tags: [Tag.JAVA]}
+    {
+      id: 0,
+      name: 'Rite Aid Project',
+      pictures: ['riteaid/image1.jpg', 'riteaid/image2.jpg', 'riteaid/image3.jpg'],
+      projectLink: '//www.riteaid.com',
+      summary:
+        'AEM based website to schedule vaccines, refill prescritions etc.',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      tags: [Tag.AEM, Tag.ANGULAR, Tag.TYPESCRIPT, Tag.HTL],
+    },
+    {
+      id: 1,
+      name: 'Ford UK Project',
+      pictures: ['forduk/photo1.jpg', 'forduk/photo2.jpg', 'forduk/photo3.jpg'],
+      projectLink: 'https://www.ford.co.uk/',
+      summary: 'Fullstack web app developed using Angular and Node.JS',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      tags: [Tag.ANGULAR, Tag.TYPESCRIPT, Tag.JAVASCRIPT],
+    }
   ];
 
-  constructor() { }
+  constructor() {}
 
-  GetProjects () {
-    return this.projects
+  GetProjects() {
+    return this.projects;
   }
-  GetProjectById (id: number): Project {
-    let projectById = this.projects.find(item => item.id === id);
-    if (projectById === undefined){
-      throw new TypeError("There is no project that matches the id " + id)
+  GetProjectById(id: number): Project {
+    let projectById = this.projects.find((item) => item.id === id);
+    if (projectById === undefined) {
+      throw new TypeError('There is no project that matches the id ' + id);
     }
-    return projectById
+    return projectById;
   }
 }
