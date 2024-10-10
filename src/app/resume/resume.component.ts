bd2dfe12c0c0ab9715ebe2d1a8518dcb48b34c75
Harsh Sharma <harshsharma0009@gmail.com>
2024-10-09 22:51:52 -0400

    updated resume section.

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,14 +1,17 @@
+import { NgIf } from '@angular/common';
 import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [AccordionModule],
+  imports: [AccordionModule, NgIf],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
+
+  isWorkExperienceOpen : boolean = false;
   constructor(private titleService: Title, private renderer : Renderer2){
     this.titleService.setTitle('Harsh Sharma - Resume')
   }
